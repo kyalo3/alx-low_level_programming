eb7ba7d6ab91fe748cde1dad5c24d5d908a4327f
kyalo3 <kyalokimeu0@gmail.com>
2023-05-30 00:49:11 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - prints a string
+ * in reverse
+ * @str: string to print
+ */
+void print_rev(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str--);
+	}
+	_putchar('\n');
+}
