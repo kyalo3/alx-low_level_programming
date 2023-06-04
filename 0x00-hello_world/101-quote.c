1bff20b0b7967fcb3ef9b579b480b1d0ca2a7d50
kyalo3 <kyalokimeu0@gmail.com>
2023-06-03 22:54:30 -0700

    Write a C program that prints a string followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
-*main-aCprogramthatprintsexactly
-*followedbyanewline,tothestandarderror.
-*Return:Always0(Success)
-*/
+ * main - a C rpogram that prints a line to the standard error
+ * Return: 1 (Success)
+ */
 int main(void)
 {
-	fputs("andthatpieceofartisuseful\"
-	-DoraKorpar,2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
