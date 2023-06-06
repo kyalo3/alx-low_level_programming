d71ab82ec27f2f170d1691f43ccf25f9abadd9e7
kyalo3 <kyalokimeu0@gmail.com>
2023-06-05 23:20:15 -0700

    Write a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,8 @@
  * @s: first string
  * @accept: second string
  * Return: a pointer to the byte in s that matches one of
- * the bytes in accept, or NULL if no such byte is found.
+ * the bytes in accept, or NULL if
+ * no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
