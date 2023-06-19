98ccf245a77e3ea926e7f233403bf4187dbc256b
kyalo3 <kyalokimeu0@gmail.com>
2023-06-19 20:21:32 +0300

    correcting betty style on the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,13 @@
  * @owner: owner name
  * Description: dog attributes
  */
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
- typedef struct dog dog_t;
+typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
