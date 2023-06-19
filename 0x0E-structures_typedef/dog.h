6a8fbc516895fbf42d051fa42812bbd3e21d4bc7
kyalo3 <kyalokimeu0@gmail.com>
2023-06-19 05:44:01 -0700

    adding dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef
+#define
+
+/**
+ * struct dog - contains ...
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owenwe name
+ * Description: dog attributes
+ */
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+#endif
