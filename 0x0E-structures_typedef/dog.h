c291668e38662b55863085158364acb20b2445e8
kyalo3 <kyalokimeu0@gmail.com>
2023-06-19 05:48:53 -0700

    Define a new type struct dog with the following elements:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - contains ...
@@ -10,8 +10,8 @@
  */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 #endif
