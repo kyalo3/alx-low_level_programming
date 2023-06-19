a87868b4efc305cdeeb38d08f6d0114f963c1944
kyalo3 <kyalokimeu0@gmail.com>
2023-06-19 20:18:16 +0300

    corrected betty errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,20 +2,20 @@
 
 /**
  * init_dog - contains ...
- * @d - pointer
+ * @d: pointer
  * @name: name of dog
  * @age: age of dog
  * @owner: owner name
  * Description: dog variables
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-        if (d){
-                d->name = name;
-                d->age = age;
-                d->owner = owner;
-        }
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
