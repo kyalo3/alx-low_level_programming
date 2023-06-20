3fd1f3746925357063395404b4ec00d2c39521e9
kyalo3 <kyalokimeu0@gmail.com>
2023-06-20 13:09:19 +0300

    new changes

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * main - entry point
+ * Description: new compiled file
+ * Return: 0
+ */
 
 int main(void)
-{	
+{
 	char *file_name = (char *)malloc(strlen(__FILE__) + 1);
 
 	printf("02-main.c %s\n", file_name);
