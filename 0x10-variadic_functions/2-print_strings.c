351dea32600a6a82efadf63fb134ce8df005f5f9
kyalo3 <kyalokimeu0@gmail.com>
2023-06-23 01:31:56 +0300

    new changes

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+ * prints_string - prints arguments in a string
+ * @separator: string to be printed between strings
+ * @n: number of strings passed
+ * Description: a function that prints strings
+ * 
+ * Return: Null
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list()
