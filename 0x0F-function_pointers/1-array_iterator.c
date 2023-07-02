cd35ed35ed15b729db39b09e8d349ff83bf4fd33
kyalo3 <kyalokimeu0@gmail.com>
2023-07-02 18:22:17 +0300

    prints name

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
