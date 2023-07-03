c547ff1699b655b6666a74c2c3652591c91984fe
kyalo3 <kyalokimeu0@gmail.com>
2023-07-03 16:52:07 +0300

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-    hello db "Hello, Holberton\n", 0\n
+    hello db "Hello, Holberton", 0
 
 section .text
     global main
@@ -9,13 +9,14 @@ main:
     ; Set up stack frame
     push rbp
     mov rbp, rsp
-
+    
     ; Print the string using printf
     mov rdi, hello
     xor eax, eax    ; Clear EAX register
     call printf
-
+    
     ; Clean up and exit
     mov eax, 0      ; Exit status 0
     pop rbp
     ret
+
