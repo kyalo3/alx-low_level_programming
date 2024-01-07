9c6c33dceefd07e9151d5f4eb95c7e3b59d08c28
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-07 22:47:52 +0300

    10. Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array of integers
+ * _binary_search - searches for a value in a sorted array of integers
  * using the binary search algorithm
  * @array: array of integers
  * @left: Left element
